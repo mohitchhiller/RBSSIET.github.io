e095a28888a2554ec3919d862117e4eaf8fb15c5
mohitchhiller <mkjangid129@gmail.com>
2021-07-14 08:20:18 +0530

    mine code

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-const openModal = document.querySelectorAll('.open-btn');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const closeModal = document.querySelector('.close-modal');
-const alertWindow = document.querySelector('.query');
-const openFn = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const navMob = document.getElementById("navdesk");
+
+const openMobNav = function () {
+  console.log("fn workin");
+  navMob.classList.toggle("hidden");
+  navMob.classList.toggle("active");
 };
-
-const closeFn = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-for (i = 0; i < openModal.length; i++) {
-  openModal[i].addEventListener('click', openFn);
-}
-
-closeModal.addEventListener('click', closeFn);
-overlay.addEventListener('click', closeFn);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') {
-    closeFn();
-  }
-});
-
-alertWindow.addEventListener('click', function () {
-  alert('Submitted !');
-});
