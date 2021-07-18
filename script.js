c6a95381f4108a87c1da10cd92f4be912ba86049
mohitchhiller <mkjangid129@gmail.com>
2021-07-18 20:11:42 +0530

    navbar update

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const navMob = document.getElementById("navdesk");
 
 const openMobNav = function () {
   console.log("fn workin");
-  navMob.classList.toggle("hidden");
   navMob.classList.toggle("active");
+  navMob.classList.toggle("hidden");
+};
+const openNav = function (x) {
+  document.querySelector(`.drop${x}`).classList.toggle("active-nav");
+  document.querySelector(`.drop${x}`).classList.toggle("inactive-nav");
 };
